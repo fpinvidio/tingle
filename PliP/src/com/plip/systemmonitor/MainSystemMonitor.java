7e204e66864aa17796528ce9875cc94ed1ff6200
marshhxx <marianaa@infocorp.com.uy>
2014-01-20 20:01:33 -0200

    detección cubeta

--- PliP/src/com/plip/systemmonitor/MainSystemMonitor.java
+++ PliP/src/com/plip/systemmonitor/MainSystemMonitor.java
@@ -16,13 +16,8 @@ import com.plip.eventhandlers.handlers.CounterEventHandler;
 import com.plip.eventhandlers.handlers.TrayEventHandler;
 import com.plip.eventhandlers.listeners.GenericEventListener;
 import com.plip.imageprocessing.processors.ObjectCounter;
-import com.plip.imageprocessing.processors.OrbBriskDetector;
 import com.plip.imageprocessing.processors.TrayProcessor;
 import com.plip.imageprocessing.processors.Exceptions.NoImageException;
-import com.plip.persistence.dao.impls.PageDaoImpl;
-import com.plip.persistence.dao.interfaces.PageDao;
-import com.plip.persistence.exceptions.PageNotFoundException;
-import com.plip.persistence.model.Page;
 import com.plip.uinterfaces.MainMenuFrame;
 
 
@@ -67,24 +62,24 @@ public class MainSystemMonitor implements GenericEventListener {
 //			Image img = (Image) it.next();
 //			System.out.println(img.getPath());
 //		}
-		ObjectCounter counter = new ObjectCounter();
-		Mat image = Highgui.imread(MainSystemMonitor.class.getResource("/49.jpg").getPath());
-		try{
-		ArrayList<Mat> foundObjects = counter.count(image);
-		PageDao pageDao = new PageDaoImpl();
-		Page page = new Page();
-		try {
-			page = pageDao.getPage(1);
-		} catch (PageNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		OrbBriskDetector orbBriskDetector = new OrbBriskDetector();
-		orbBriskDetector.recognize(page, foundObjects);
-		}catch(NoImageException e){
-			e.printStackTrace();
-		}
-	}
+//		ObjectCounter counter = new ObjectCounter();
+//		Mat image = Highgui.imread(MainSystemMonitor.class.getResource("/49.jpg").getPath());
+//		try{
+//		ArrayList<Mat> foundObjects = counter.count(image);
+//		PageDao pageDao = new PageDaoImpl();
+//		Page page = new Page();
+//		try {
+//			page = pageDao.getPage(1);
+//		} catch (PageNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		OrbBriskDetector orbBriskDetector = new OrbBriskDetector();
+//		orbBriskDetector.recognize(page, foundObjects);
+//		}catch(NoImageException e){
+//			e.printStackTrace();
+//		}
+}
 
 	/*CvCapture *cap;
 	int n = 0;
@@ -100,9 +95,9 @@ public class MainSystemMonitor implements GenericEventListener {
 	*/
 	public void initializeCapture() {
 
-		vcapture = new VideoCapture(1);
-		vcapture.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, 200);
-		vcapture.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, 250);
+		vcapture = new VideoCapture(0);
+		vcapture.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, 640);
+		vcapture.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, 480);
 		tprocessor = new TrayProcessor();
 		ocounter = new ObjectCounter();
 		tehandler = new TrayEventHandler();
@@ -132,16 +127,24 @@ public class MainSystemMonitor implements GenericEventListener {
 	public void handleEvent(EventObject event) {
 		if (event instanceof TrayArrivalEvent) {
 			System.out.println("Arrival");
-			vcapture.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, 2592);
-			vcapture.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, 1936);
+			//1620 X 1080			
+			vcapture.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, 1620);
+			vcapture.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, 1080);
+			
+			try {
+			    Thread.sleep(400);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}
+			
 			Mat screenshot = new Mat(); 
 			vcapture.read(screenshot);
-			while(screenshot.empty()) {
-				vcapture.read(screenshot);
-			}
-			vcapture.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, 200);
-			vcapture.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, 250);
+			
 			Highgui.imwrite("Tray.jpg", screenshot);
+			
+			vcapture.set(Highgui.CV_CAP_PROP_FRAME_WIDTH, 640);
+			vcapture.set(Highgui.CV_CAP_PROP_FRAME_HEIGHT, 480);
+			
 			TrayArrivalEvent temp = (TrayArrivalEvent) event;
 			ArrayList<Mat> images = new ArrayList<Mat>();
 			try{
