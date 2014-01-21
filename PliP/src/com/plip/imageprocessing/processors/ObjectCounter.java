62bf9306a2eac595566e1a990c2d1031c7c002f2
Martin Perez Invidio <mperez@cualit.com>
2014-01-21 19:56:00 -0200

    *ObjectDetector cuenta y elimina cubeta

--- PliP/src/com/plip/imageprocessing/processors/ObjectCounter.java
+++ PliP/src/com/plip/imageprocessing/processors/ObjectCounter.java
@@ -32,7 +32,9 @@ public class ObjectCounter {
 		if (image != null) {
 			ArrayList<Mat> resultImages = new ArrayList<Mat>();
 			Mat imageWithEdges = edgeDetector(image);
-			Mat detectedObjects = findContours(imageWithEdges);
+			Mat noTray = removeTray(imageWithEdges);
+			Mat detectedObjects = findContours(noTray);
+			imageDistanceTransformer(detectedObjects);
 			System.out.println("Quantity:" + quantity);
 			System.out.println("Contours size:" + this.contours.size());
 			TrayStatusDaoImpl trayStatusDao = new TrayStatusDaoImpl();
@@ -74,16 +76,35 @@ public class ObjectCounter {
 			MatOfPoint mMOP = new MatOfPoint();
 			Scalar s1 = new Scalar(255, 0, 0);
 			int count = 0;
-
+			Highgui.imwrite("noTray.jpg", filteredImage);
 			/* Finds contours in the image and saves contours to contours object */
 			Imgproc.findContours(filteredImage, contours, new Mat(),
 					Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
 			Imgproc.drawContours(filteredImage, contours, -1, s1, -1);
+//			Imgproc.findContours(filteredImage, contours, new Mat(),
+//					Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_NONE);
+//			for (int i = 0; i < contours.size(); i++) {
+//				contours.get(i).convertTo(mMOP2f1, CvType.CV_32FC2);
+//				Imgproc.approxPolyDP(mMOP2f1, approx,
+//				Imgproc.arcLength(mMOP2f1, true) * 0.02, true);
+//				approx.convertTo(mMOP, CvType.CV_32S); 
+//				if(Imgproc.contourArea(approx)<400000 && Imgproc.arcLength(mMOP2f1, true)<1000){
+//					Imgproc.drawContours(filteredImage, contours, i, s1, -1);
+//				}else if(Imgproc.contourArea(approx)>500000){
+//					filteredImage=cropContour(filteredImage, contours.get(i), i);
+//				}else{
+//					Imgproc.drawContours(filteredImage, contours, i, new Scalar(0,0,0),2);
+//				}
+//			
+//			}
+//			Imgproc.threshold(filteredImage, filteredImage, 50, 255, Imgproc.THRESH_BINARY);
+			Highgui.imwrite("contours.jpg", filteredImage);
+//			Imgproc.erode(filteredImage, filteredImage, new Mat(), new Point(-1, -1), 5);
+//					Highgui.imwrite("contourse.jpg", filteredImage);
+			Imgproc.dilate(filteredImage, filteredImage, new Mat(),
+					new Point(-1, -1), 2);
 
-			// Highgui.imwrite("contours.jpg", filteredImage);
-			Imgproc.erode(filteredImage, filteredImage, new Mat(), new Point(
-					-1, -1), 5);
-			// Highgui.imwrite("contourse.jpg", filteredImage);
+			 Highgui.imwrite("contourse.jpg", filteredImage);
 			contours = new ArrayList<MatOfPoint>();
 			Imgproc.findContours(filteredImage, contours, new Mat(),
 					Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
@@ -105,15 +126,16 @@ public class ObjectCounter {
 				 * Draw contours between an estimated area, to avoid to big or
 				 * to small objects
 				 */
-				if (Imgproc.contourArea(verticesMat) > 40000
-						&& Imgproc.contourArea(verticesMat) < 6000000) {
+				if (Imgproc.contourArea(verticesMat) > 15000
+						&& Imgproc.contourArea(verticesMat) < 450000) {
 
-					// System.out.println(Imgproc.contourArea(verticesMat));
+//					 System.out.println(Imgproc.contourArea(verticesMat));
 					Imgproc.drawContours(filteredImage, contours, i, s1, -1);
 					this.contours.add(contours.get(i));
-					// Highgui.imwrite("contour"+i+".jpg", filteredImage);
+					 Highgui.imwrite("contour"+i+".jpg", filteredImage);
 					quantity++;
 				}
+					
 			}
 			Highgui.imwrite("contoursfilled.jpg", filteredImage);
 			return filteredImage;
@@ -134,9 +156,10 @@ public class ObjectCounter {
 		Imgproc.findContours(filteredImage, contours, new Mat(),
 				Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
 		quantity = contours.size();
-		// Highgui.imwrite("contoursT.jpg", filteredImage);
-		// Imgproc.drawContours(filteredImage, contours, -1, s1, -1);
-		// Highgui.imwrite("contoursfilledT.jpg", filteredImage);
+		this.contours = contours;
+//		 Highgui.imwrite("contoursT.jpg", filteredImage);
+		 Imgproc.drawContours(filteredImage, contours, -1, s1, -1);
+		 Highgui.imwrite("contoursfilledT.jpg", filteredImage);
 		return filteredImage;
 	}
 
@@ -191,10 +214,10 @@ public class ObjectCounter {
 			// Highgui.imwrite("normalize.jpg", normalize);
 
 			/* Threshold to recount the objects */
-			Imgproc.threshold(normalize, thres, 64, 128, Imgproc.THRESH_BINARY);
+			Imgproc.threshold(normalize, thres, 64, 255, Imgproc.THRESH_BINARY);
 			// Highgui.imwrite("thres.jpg", thres);
 			Imgproc.dilate(thres, thres, new Mat(), new Point(-1, -1), 10);
-			// Highgui.imwrite("thresd.jpg", thres);
+			 Highgui.imwrite("thresd.jpg", thres);
 
 			thres.convertTo(thres, CvType.CV_8UC1);
 
@@ -207,18 +230,81 @@ public class ObjectCounter {
 	public Mat cropContour(Mat image, MatOfPoint contour, int i) {
 		if (image != null && contour != null) {
 			Rect boundRect = Imgproc.boundingRect(contour);
-			boundRect.height += 30;
-			boundRect.width += 30;
-			boundRect.x -= 15;
-			boundRect.y -= 15;
+//			boundRect.height += 30;
+//			boundRect.width += 30;
+//			boundRect.x -= 15;
+//			boundRect.y -= 15;
+//			boundRect.height -= 30;
+//			boundRect.width -= 30;
+//			boundRect.x += 15;
+//			boundRect.y += 15;
+			if(boundRect.x < 0){
+			   boundRect.x = 0;
+			}
+			if(boundRect.y < 0){
+				   boundRect.y = 0;
+			  }
+			if(boundRect.width + boundRect.x > image.cols()){
+				   boundRect.width = Math.abs(image.cols() - boundRect.x); 
+			 }
+			if(boundRect.height + boundRect.y > image.rows()){
+				   boundRect.height = Math.abs(image.rows() - boundRect.y); 
+			 }
+			
 			Mat subImage = image.submat(boundRect);
-			String filename = getClass().getResource("/FoundObjects").getPath()
-					+ "/bound" + i + ".jpg";
-			Highgui.imwrite(filename, subImage);
+//			String filename = getClass().getResource("/FoundObjects").getPath()
+//					+ "/bound" + i + ".jpg";
+//			Highgui.imwrite(filename, subImage);
 
 			return subImage;
 		} else {
 			return new Mat();
 		}
 	}
+	
+	public Mat removeTray(Mat image){
+		if(image != null){
+			Highgui.imwrite("noCrop.jpg", image);	
+		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+		MatOfPoint trayFloorContour = new MatOfPoint();
+		boolean initialized = false;
+		MatOfPoint2f approx = new MatOfPoint2f();
+		MatOfPoint2f mMOP2f1 = new MatOfPoint2f();
+		MatOfPoint mMOP = new MatOfPoint();
+		Scalar s1 = new Scalar(255, 0, 0);
+		Imgproc.findContours(image, contours, new Mat(),
+				Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_NONE);
+		for (int i = 0; i < contours.size(); i++) {
+			contours.get(i).convertTo(mMOP2f1, CvType.CV_32FC2);
+			RotatedRect rotated = Imgproc.minAreaRect(mMOP2f1);
+			Point[] vertices = new Point[4];
+			rotated.points(vertices);
+			MatOfPoint2f verticesMat = new MatOfPoint2f();
+			verticesMat.fromArray(vertices);
+			 if(Imgproc.contourArea(verticesMat)>450000){
+				Imgproc.drawContours(image, contours, i, new Scalar(0), 2); 
+////				image=cropContour(image, contours.get(i), i);
+				 if(!initialized){
+					trayFloorContour = contours.get(i);
+					initialized = true;
+				 }else{
+				    Rect boundRect = Imgproc.boundingRect(contours.get(i));
+				    Rect floor = Imgproc.boundingRect(trayFloorContour);
+				    if(boundRect.x > floor.x && boundRect.y <= floor.y){
+				    	trayFloorContour = contours.get(i);
+				    }
+				 }
+		}else{
+			Imgproc.drawContours(image, contours, i, s1, 2);
+		}
+	}
+		 if(initialized){
+			 image=cropContour(image, trayFloorContour, 1);
+			 }
+		Imgproc.threshold(image, image, 100, 255, Imgproc.THRESH_BINARY); 
+		return image;
+		}
+		return new Mat();
 }
+
+}	
